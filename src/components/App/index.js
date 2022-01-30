62ee9ba5a1b675b2da3e814f590dfc463102a1c3
adam960 <91432921+ah960@users.noreply.github.com>
2022-01-30 21:42:18 +0000

    ui library comp added

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,13 +2,15 @@ import css from "./app.module.css";
 import articles from "../../libs/articles";
 import Header from "../Header";
 import ArticleList from "../ArticleList";
+import LibDemo from "../LibDemo";
+
 function App() {
   return (
     <main className={css.app}>
       <Header />
       <ArticleList articles={articles} />
+      <LibDemo />
     </main>
   );
 }
-
 export default App;
