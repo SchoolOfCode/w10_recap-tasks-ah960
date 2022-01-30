dbdca48c712ce10e11b0ed63e1f6b02f7d55af6e
adam960 <91432921+ah960@users.noreply.github.com>
2022-01-30 19:43:04 +0000

    -broke down a bit and list drawn

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,31 +1,13 @@
 import "./App.css";
 import articles from "../../libs/articles";
-
+import Header from "../Header";
+import ArticleList from "../ArticleList";
 function App() {
   return (
     <main className="App">
-      <h1>WikiPigeon</h1>
-      {articles.map((article) => {
-        return (
-          <article key={article.id} className="post">
-            <h2>{article.title}</h2>
-            {article.paragraphs.map((paragraph) => (
-              <p key={paragraph.id}>{paragraph.text}</p>
-            ))}
-            <button className="like-button">Like 👍</button>
-            <section className="comment-section">
-              {article.comments.map((comment) => {
-                return (
-                  <div key={comment.id} className="comment">
-                    <h4>{comment.name} says:</h4>
-                    <p>{comment.text}</p>
-                  </div>
-                );
-              })}
-            </section>
-          </article>
-        );
-      })}
+      <Header />
+      <ArticleList articles={articles} />
+      <h1>PLACEHOLDER MY CODE ABOVE</h1>
     </main>
   );
 }
