1192496cc548fffaab6ce74134d993319fb3ac56
adam960 <91432921+ah960@users.noreply.github.com>
2022-01-30 22:00:18 +0000

    -auth0 attempt 1

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,11 +3,18 @@ import articles from "../../libs/articles";
 import Header from "../Header";
 import ArticleList from "../ArticleList";
 import LibDemo from "../LibDemo";
+import LoginButton from "../LogInButton";
+import LogoutButton from "../LogoutButton";
+import React from "react";
+import LogInContent from "../LogInContent";
 
 function App() {
   return (
     <main className={css.app}>
       <Header />
+      <LoginButton />
+      <LogoutButton />
+      <LogInContent />
       <ArticleList articles={articles} />
       <LibDemo />
     </main>
